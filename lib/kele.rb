d280e19acb59f9b52b06e46ff2b60a677992fc41
George Bumgardner <bumgardnera07@gmail.com>
2017-11-12 02:33:24 +0000

    user data

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
+    include JSON
     
     def initialize(email, password)
         options = {
@@ -11,13 +13,19 @@ class Kele
                 }
         }
         @uri = 'https://www.bloc.io/api/v1'
-        @response = self.class.post(@uri + '/sessions', options)
-        @auth_token = @response.parsed_response["auth_token"]
+        response = self.class.post(@uri + '/sessions', options)
+        @auth_token = response.parsed_response["auth_token"]
         
         if @auth_token == nil
             puts "invalid credentials, please try again"
         else 
             puts "you're in"
         end
+    end
+    
+    def get_me
+        url = @uri + "/users/me"
+        response = self.class.get(url, headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end
 end
